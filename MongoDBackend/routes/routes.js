78a8902266062e8331338901b71e9cab4e8f2d0b
Abhishek Madhukar Mahajan <abhishekmahajan3711@gmail.com>
2024-03-24 18:14:36 +0530

    Added routes for GetReport1 and GetReport2 functions

--- MongoDBackend/routes/routes.js
+++ MongoDBackend/routes/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const routers=express.Router();
 import {CreateUser,ValidateUser} from "../controller/UserController.js";
-import {AddReport1,AddReport2,AddReport3} from "../controller/ReportControllers.js";
+import {AddReport1,AddReport2,AddReport3,GetReport1,GetReport2} from "../controller/ReportControllers.js";
 import {AddUserConversation1,AddUserConversation2,AddUserConversation3} from "../controller/UserInputControllers.js"
 
 routers.post('/signup',CreateUser);
@@ -12,5 +12,7 @@ routers.post('/report3_desc',AddReport3);
 routers.post('/userConv1',AddUserConversation1);
 routers.post('/userConv2',AddUserConversation2);
 routers.post('/userConv3',AddUserConversation3);
+routers.get('/get_report1_desc',GetReport1);
+routers.get('/get_report2_desc',GetReport2);
 
 export default routers;
